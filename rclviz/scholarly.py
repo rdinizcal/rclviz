6957d5c9f3552dcec810ad93f76fe57c75738fb8
Ricardo Caldas <18559295+rdinizcal@users.noreply.github.com>
2023-04-24 21:21:28 +0200

    create scholarly.py

---
+++ rclviz/scholarly.py
@@ -0,0 +1,25 @@
+from typing import Dict
+from tqdm import tqdm
+from scholarly import scholarly
+from rclviz.location import fetch_location
+
+def clean_affiliation(affiliation : str) -> str:
+    separator = ','
+    affiliation= affiliation.split(separator,1)[1] if separator in affiliation else affiliation
+    separator = ' at '
+    affiliation= affiliation.split(separator,1)[1] if separator in affiliation else affiliation
+    separator = ' and '
+    affiliation= affiliation.split(separator,1)[0] if separator in affiliation else affiliation
+    return affiliation
+
+def get_coauthors(name: str) -> Dict[str, str]:
+    search_query = scholarly.search_author(name)
+    author_dict = next(search_query)
+    author = scholarly.fill(author_dict)
+    coauthors = {}
+    for coauthor in tqdm(author['coauthors'], desc="Fetching coauthors"):
+        coauthor_dict = next(scholarly.search_author(coauthor['name']))
+        coauthor = scholarly.fill(coauthor_dict)
+        affiliation = clean_affiliation(coauthor['affiliation'])  # cleans affiliation if it has a comma
+        coauthors[coauthor['name']] = affiliation
+    return coauthors
