3095140e5dfaf5906c4a977144ea27e350a3a9bd
Ricardo Caldas <18559295+rdinizcal@users.noreply.github.com>
2023-04-24 21:21:58 +0200

    Create location.py

---
+++ rclviz/location.py
@@ -0,0 +1,10 @@
+from geopy.geocoders import Nominatim
+
+def fetch_location(university: str) -> Dict[str, float]:
+    """Fetches the latitude and longitude of a university using geopy."""
+    geolocator = Nominatim(user_agent="world_academic_collab")
+    location = geolocator.geocode(university)
+    if not location: 
+        print("[WARN] Did not find " + str(university) + ". Excluding connection from plot.")
+        return {'lat': 0, 'lng': 0}
+    else: return {'lat': location.latitude, 'lng': location.longitude}
