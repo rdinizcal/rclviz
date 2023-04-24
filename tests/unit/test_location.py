531be8f6a515eeeff95e5bb2326ab84714476191
Ricardo Caldas <ricardo.caldas@chalmers.se>
2023-04-24 23:13:37 +0200

    fixed tests

--- tests/unit/test_location.py
+++ tests/unit/test_location.py
@@ -1,41 +1,42 @@
-import unittest
+import pytest
 from unittest.mock import patch, Mock
-from location import fetch_location, LocationNotFoundError
+from rclviz.location import fetch_location, LocationNotFoundError
 
-class TestLocation(unittest.TestCase):
-    
-    @patch('location.Nominatim')
-    def test_fetch_location(self, mock_nominatim):
-        mock_geolocator = mock_nominatim.return_value
-        mock_location = Mock()
-        mock_location.latitude = 40.7128
-        mock_location.longitude = -74.0060
-        mock_geolocator.geocode.return_value = mock_location
-        
-        location = fetch_location("New York University")
-        
-        self.assertEqual(location, {'lat': 40.7128, 'lng': -74.0060})
-        mock_geolocator.geocode.assert_called_once_with("New York University")
-        
-    @patch('location.Nominatim')
-    def test_fetch_location_not_found(self, mock_nominatim):
-        mock_geolocator = mock_nominatim.return_value
-        mock_geolocator.geocode.return_value = None
-        
-        with self.assertRaises(LocationNotFoundError):
-            fetch_location("Nonexistent University")
-            
-        mock_geolocator.geocode.assert_called_once_with("Nonexistent University")
+@pytest.fixture
+def mock_nominatim():
+    with patch('rclviz.location.Nominatim') as mock_nominatim:
+        yield mock_nominatim
 
-    def test_fetch_location_additional():
-        # Test University of Pernambuco
-        result = location.fetch_location("University of Pernambuco")
-        assert result == {'lat': -8.0278246, 'lng': -34.9505235}
+def test_fetch_location(mock_nominatim):
+    mock_geolocator = mock_nominatim.return_value
+    mock_location = Mock()
+    mock_location.latitude = 40.7128
+    mock_location.longitude = -74.0060
+    mock_geolocator.geocode.return_value = mock_location
 
-        # Test University of Singapore
-        result = location.fetch_location("University of Singapore")
-        assert result == {'lat': 1.2965886, 'lng': 103.7766665}
+    location = fetch_location("New York University")
 
-        # Test University of Madagascar
-        with pytest.raises(location.LocationNotFoundError):
-            location.fetch_location("University of Madagascar")
+    assert location == {'lat': 40.7128, 'lng': -74.0060}
+    mock_geolocator.geocode.assert_called_once_with("New York University")
+
+def test_fetch_location_not_found(mock_nominatim):
+    mock_geolocator = mock_nominatim.return_value
+    mock_geolocator.geocode.return_value = None
+
+    with pytest.raises(LocationNotFoundError):
+        fetch_location("Nonexistent University")
+
+    mock_geolocator.geocode.assert_called_once_with("Nonexistent University")
+
+def test_fetch_location_additional():
+    # Test University of Pernambuco
+    result = fetch_location("University of Pernambuco")
+    assert result == {'lat': -8.052423600000001, 'lng': -34.9510615050235}
+
+    # Test University of Singapore
+    result = fetch_location("University of Singapore")
+    assert result == {'lat': 1.2962018, 'lng': 103.77689943784759}
+
+    # Test University of Madagascar
+    with pytest.raises(LocationNotFoundError):
+        fetch_location("University of Madagascar")
