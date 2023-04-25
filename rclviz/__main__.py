ce3e9fb50ab5bf103b3554f3cf7c56937b92adbe
Ricardo Caldas <ricardo.caldas@chalmers.se>
2023-04-25 09:32:38 +0200

    fix exception handling and performance

--- rclviz/__main__.py
+++ rclviz/__main__.py
@@ -1,12 +1,12 @@
 import sys
 from kml import generate_kml_file
-from location import fetch_location
+from location import fetch_location, LocationNotFoundError
 from scholar import get_coauthors
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print('Please provide your name and your university as command line arguments.')
-        print('Example usage: python main.py "John Doe" "University of California, Los Angeles"')
+        print('Example usage: python main.py "Paulo Freire" "Universidade de Pernambuco"')
         sys.exit()
 
     name = sys.argv[1]
@@ -17,19 +17,14 @@ if __name__ == '__main__':
         print('Please provide valid non-empty strings for your name and your university.')
         sys.exit()
 
-    # Get coauthors
-    coauthors = get_coauthors(name)
-
-    if not coauthors:
-        print('Error: Could not find any coauthors for the given name.')
-        sys.exit()
-    
-    # Get location
     try:
-        fetch_location(university)
+        fetch_location(university) # Get location
+        coauthors = get_coauthors(name) # Get coauthors
     except LocationNotFoundError as e:
         print(f"Error: {str(e)}")
-        sys.exit() # skip this university and move on to the next one
-
-    # Generate KML file
-    generate_kml_file(name, university, coauthors)
+        sys.exit()
+    except StopIteration as e:
+        print(f"Error: {str(e)}")
+        sys.exit()
+    
+    generate_kml_file(name, university, coauthors) # Generate KML file
